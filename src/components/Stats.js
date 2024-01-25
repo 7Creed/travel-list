ad9c279b260951b44c13a3b29773aa0f5469e079
7Creed <4chimabika@gmail.com>
2024-01-26 00:57:50 +0100

    first commit

---
+++ src/components/Stats.js
@@ -0,0 +1,23 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  // Derived state
+  const numItems = items.length;
+  const numpacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numpacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You go t everything! Ready to go ✈"
+          : ` 👜You have ${numItems} items on your list, and you already packed ${numpacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
